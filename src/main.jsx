98cd22ff0703b5dd2bce167e63d638d8e75fafca
lperlo <luchi.perlo3108@gmail.com>
2026-08-23 12:01:59 -0300

    Fix import case for salime-app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "../Salime-App.jsx";
+import App from "../salime-app.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
